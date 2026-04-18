47c646c3883423ad79c911d20acfc588e8343057
Moises Pantoja <moisepan@gmail.com>
2026-04-18 11:48:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,82 @@
-const calendar = document.querySelector("#app-calendar");
 
-const isWeekend = day =>{
-    return true;
+
+
+
+checkList()
+const myImage = document.querySelector("img");
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `${myName} is cool, `;
+  }
 }
 
-for(let day =1; day<= 31; day++ ){
-    const weekend = isWeekend(day)
-    calendar.insertAdjacentHTML("beforeend", '<div class="day ${weekend ? "weekend" : "}">${day}</div>');
-    
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Hello, ${storedName}`;
+}
+
+
+reqFlowers();
+
+
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+
+
+
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  const consoleLog = document.querySelector("p");
+  if (mySrc === "images/bae.png") {
+    myImage.setAttribute("src", "images/curl.png");
+  } else {
+    myImage.setAttribute("src", "images/bae.png");
+  }
+});
+
+
+function checkList(){
+  const listItems = document.querySelectorAll("li");
+  listItems.forEach((item) => {
+    item.addEventListener("click", (e)=>{
+      if (!e.target.className) {
+          e.target.className = "done";
+      } else {
+          e.target.className = "";
+      }
+    });
+  });
+}
+function reqFlowers(){
+  const flw = document.querySelector("img");
+  const myRequest = new Request("images/flowers.jpg");
+
+  fetch(myRequest)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error, status = ${response.status}`);
+      }
+      return response.blob();
+    })
+    .then((myBlob) => {
+      const objectURL = URL.createObjectURL(myBlob);
+      flw.src = objectURL;
+    })
+    .catch((error) => {
+      const p = document.createElement("p");
+      p.appendChild(document.createTextNode(`Error: ${error.message}`));
+      document.body.insertBefore(p, flw);
+    });
 }
